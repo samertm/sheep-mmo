f81beb364ca0f6a112198f20643c53382dee9028
Samer Masterson <samer@samertm.com>
2014-08-11 14:36:05 -0400

    Added flags

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"strconv"
 
 	"github.com/samertm/sheep-mmo/server"
 )
 
 func main() {
-	fmt.Println("Listening on ws://localhost:4977")
-	server.ListenAndServe("localhost:4977")
+	hostname := flag.String("hostname", "localhost", "the hostname")
+	port := flag.Int("port", 4977, "the port")
+	flag.Parse()
+	fmt.Printf("Listening on ws://%s:%d", *hostname, *port)
+	server.ListenAndServe((*hostname) + ":" + strconv.Itoa(*port))
 }
